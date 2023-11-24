c6eddc9ad2611aa4c32a4760de0ad8ca333a83e4
jmaczan <jedrzejpawel@maczan.pl>
2023-11-24 13:19:37 +0100

    Allocate matrix in Jacobian

--- src/linear_algebra/jacobian.py
+++ src/linear_algebra/jacobian.py
@@ -1,4 +1,9 @@
 def jacobian(vector_1, vector_2, derivative_function):
+    jacobian_matrix = [[0 for _ in range(len(vector_1))] for _ in range(len(vector_2))]
     for i in vector_1:
         for j in vector_2:
-            derivative_function(vector_1, vector_2, vector_1_index=i, vector_2_index=j)
+            jacobian_matrix[i][j] = derivative_function(
+                vector_1, vector_2, vector_1_index=i, vector_2_index=j
+            )
+
+    return jacobian_matrix
